414074f36d712ed757df1583ee6a82939173ee06
zhdanila <daniazhurba2004@gmail.com>
2024-09-17 10:40:36 +0300

    Implement logic

---
+++ JonDou9000.TaskPlanner/JonDou9000.TaskPlanner.Domain.Models/WorkItem.cs
@@ -0,0 +1,15 @@
+﻿public class WorkItem
+{
+    public DateTime CreationDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public Priority Priority { get; set; }
+    public Complexity Complexity { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Title}: due {DueDate:dd.MM.yyyy}, {Priority.ToString().ToLower()} priority";
+    }
+}
