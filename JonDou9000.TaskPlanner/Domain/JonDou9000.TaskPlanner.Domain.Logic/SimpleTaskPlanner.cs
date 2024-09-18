a1b8c0c0aa0b38bc3fc0c9c996606a88d14ad0c9
zhdanila <daniazhurba2004@gmail.com>
2024-09-18 10:44:13 +0300

    Task 4

--- JonDou9000.TaskPlanner/Domain/JonDou9000.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
+++ JonDou9000.TaskPlanner/Domain/JonDou9000.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -1,27 +1,40 @@
-﻿using JonDou9000.TaskPlanner.Domain.JonDou9000.TaskPlanner.Domain.Models;
-using System.Collections.Generic;
-using System.Linq;
+﻿using JonDou9000.TaskPlanner.DataAccess.JonDou9000.TaskPlanner.DataAccess.Abstractions;
+using JonDou9000.TaskPlanner.Domain.JonDou9000.TaskPlanner.Domain.Models;
 
 public class SimpleTaskPlanner
 {
-    public WorkItem[] CreatePlan(WorkItem[] items)
+    private readonly IWorkItemsRepository _repository;
+
+    // Конструктор, що приймає репозиторій
+    public SimpleTaskPlanner(IWorkItemsRepository repository)
     {
+        _repository = repository;
+    }
+
+    // Створення плану задач
+    public WorkItem[] CreatePlan()
+    {
+        var items = _repository.GetAll(); // Отримуємо всі задачі з репозиторія
+
+        if (items.Length == 0)
+        {
+            return Array.Empty<WorkItem>();
+        }
+
         var itemsAsList = items.ToList();
         itemsAsList.Sort(CompareWorkItems);
         return itemsAsList.ToArray();
     }
 
+    // Порівняння задач
     private static int CompareWorkItems(WorkItem firstItem, WorkItem secondItem)
     {
-        // Порівняння за Priority (спаданням)
         int priorityComparison = secondItem.Priority.CompareTo(firstItem.Priority);
         if (priorityComparison != 0) return priorityComparison;
 
-        // Порівняння за DueDate (зростанням)
         int dueDateComparison = firstItem.DueDate.CompareTo(secondItem.DueDate);
         if (dueDateComparison != 0) return dueDateComparison;
 
-        // Порівняння за Title (алфавітний порядок)
         return string.Compare(firstItem.Title, secondItem.Title, StringComparison.OrdinalIgnoreCase);
     }
 }
